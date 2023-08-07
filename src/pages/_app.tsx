7910555945607c2220a19217d4929de724168281
Mohamed Riyad Bouzani <mriyadbouzani@gmail.com>
2023-08-07 21:11:38 +0100

    reverted changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
+import { api } from "~/utils/api";
 import "~/styles/globals.css";
 import { Toaster } from "components/ui/toaster";
 
@@ -16,4 +17,4 @@ const MyApp: AppType<{ session: Session | null }> = ({
   );
 };
 
-
+export default api.withTRPC(MyApp);
